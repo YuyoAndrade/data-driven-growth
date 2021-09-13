6dee3b8c086251a36dbde7cb0ed044de5c882649
Eugenio Andrade <euyuyo01@gmail.com>
2021-09-13 12:24:37 -0500

    Finished Frequency, change functions to a file

--- segmentation/recency.py
+++ segmentation/recency.py
@@ -1,6 +1,6 @@
 from __future__ import division
 
-from percentiles import *
+from functions import *
 
 # import libraries
 from datetime import datetime, timedelta
@@ -64,17 +64,6 @@ plt.show()
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(tx_user[['Recency']])
 tx_user['RecencyCluster'] = kmeans.predict(tx_user[['Recency']])
-
-# Function for ordering cluster numbers
-def order_cluster(cluster_field_name, target_field_name,df,ascending):
-    new_cluster_field_name = 'new_' + cluster_field_name
-    df_new = df.groupby(cluster_field_name)[target_field_name].mean().reset_index()
-    df_new = df_new.sort_values(by=target_field_name,ascending=ascending).reset_index(drop=True)
-    df_new['index'] = df_new.index
-    df_final = pd.merge(df,df_new[[cluster_field_name,'index']], on=cluster_field_name)
-    df_final = df_final.drop([cluster_field_name],axis=1)
-    df_final = df_final.rename(columns={"index":cluster_field_name})
-    return df_final
 
 tx_user = order_cluster('RecencyCluster', 'Recency',tx_user,False)
 
